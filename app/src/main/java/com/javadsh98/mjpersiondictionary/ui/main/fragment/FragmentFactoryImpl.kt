f043c606319e24e7b9ffecc68c6330b81d530c50
MJavad98 <mohammad.javad.shabani.1377.3.24@gmail.com>
2020-05-29 01:07:58 +0430

    Use Android Navigation Architecture Component

---
+++ app/src/main/java/com/javadsh98/mjpersiondictionary/ui/main/fragment/FragmentFactoryImpl.kt
@@ -0,0 +1,24 @@
+package com.javadsh98.mjpersiondictionary.ui.main.fragment
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+import com.javadsh98.mjpersiondictionary.ui.main.fragment.History.HistoryFragment
+import com.javadsh98.mjpersiondictionary.ui.main.fragment.favorite.FavoriteFragment
+import com.javadsh98.mjpersiondictionary.ui.main.fragment.home.HomeFragment
+import com.javadsh98.mjpersiondictionary.ui.main.fragment.more.MoreFragment
+
+
+class FragmentFactoryImpl : FragmentFactory() {
+
+    override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
+        return when(className) {
+
+            HomeFragment::class.java.name -> HomeFragment()
+            FavoriteFragment::class.java.name -> FavoriteFragment()
+            HistoryFragment::class.java.name -> HistoryFragment()
+            MoreFragment::class.java.name -> MoreFragment()
+
+            else -> super.instantiate(classLoader, className)
+        }
+    }
+}
