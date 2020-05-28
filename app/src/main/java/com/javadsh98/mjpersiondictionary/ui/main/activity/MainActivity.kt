a96fd6d25a7d7a663a0d7dfb4843ffecad3a867f
MJavad98 <mohammad.javad.shabani.1377.3.24@gmail.com>
2020-05-29 01:04:49 +0430

    Use Android Navigation Architecture Component

--- app/src/main/java/com/javadsh98/mjpersiondictionary/ui/main/activity/MainActivity.kt
+++ app/src/main/java/com/javadsh98/mjpersiondictionary/ui/main/activity/MainActivity.kt
@@ -1,36 +1,20 @@
 package com.javadsh98.mjpersiondictionary.ui.main.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.View
-import android.view.ViewManager
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.javadsh98.mjpersiondictionary.R
 import com.javadsh98.mjpersiondictionary.databinding.ActivityMainBinding
 import com.javadsh98.mjpersiondictionary.ui.main.fragment.FragmentFactoryImpl
-import com.javadsh98.mjpersiondictionary.ui.main.fragment.favorite.FavoriteFragment
-import com.javadsh98.mjpersiondictionary.ui.main.fragment.History.HistoryFragment
-import com.javadsh98.mjpersiondictionary.ui.main.fragment.home.HomeFragment
-import com.javadsh98.mjpersiondictionary.ui.main.fragment.more.MoreFragment
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
 
-class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(){
 
     lateinit var binding: ActivityMainBinding
     lateinit var root: View
-
-    //fragments
-    lateinit var  homeFragment: HomeFragment
-    lateinit var  favoriteFragment: FavoriteFragment
-    lateinit var  historyFragment: HistoryFragment
-    lateinit var  moreFragment: MoreFragment
-
-    //viewmodel
-    lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         //following code must before super.onCreate(savedInstanceState)
@@ -41,80 +25,16 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         root = binding.root
         setContentView(root)
 
+        val navController = findNavController(R.id.container_main_fragment)
+        //connect bottomnavigation with navController
+        bottomnavigation_main_menu.setupWithNavController(navController)
 
-        initfragments()
-        setupViewmodel()
-        setupBottomNavigation()
+        //connect toolbar with navController
+        setSupportActionBar(toolbar_main)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.favoriteFragment
+            , R.id.historyFragment, R.id.moreFragment))
+        toolbar_main.setupWithNavController(navController, appBarConfiguration)
 
-    }
-
-    private fun setupBottomNavigation() {
-        //use viewmodel position
-        when(viewModel.getPosition()) {
-            BottomPosition.HOME -> {
-                bottomnavigation_main_menu.selectedItemId = R.id.menu_home
-                setupFragment(homeFragment)
-            }
-            BottomPosition.FAVORITE -> {
-                bottomnavigation_main_menu.selectedItemId = R.id.menu_favorite
-                setupFragment(favoriteFragment)
-            }
-            BottomPosition.HISTORY -> {
-                bottomnavigation_main_menu.selectedItemId = R.id.menu_history
-                setupFragment(historyFragment)
-            }
-            BottomPosition.MORE -> {
-                bottomnavigation_main_menu.selectedItemId = R.id.menu_more
-                setupFragment(moreFragment)
-            }
-        }
-        //listener
-        root.bottomnavigation_main_menu.setOnNavigationItemSelectedListener(this)
-    }
-
-    private fun setupViewmodel() {
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        var id = item.itemId
-
-        when(id){
-            R.id.menu_home -> {
-                setupFragment(homeFragment)
-                viewModel.setPosition(BottomPosition.HOME)
-            }
-            R.id.menu_favorite -> {
-                setupFragment(favoriteFragment)
-                viewModel.setPosition(BottomPosition.FAVORITE)
-            }
-            R.id.menu_history -> {
-                setupFragment(historyFragment)
-                viewModel.setPosition(BottomPosition.HISTORY)
-            }
-            R.id.menu_more -> {
-                setupFragment(moreFragment)
-                viewModel.setPosition(BottomPosition.MORE)
-            }
-        }
-        return true
-    }
-
-    fun setupFragment(fragment: Fragment){
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container_main_fragment, fragment).commit()
-    }
-
-    fun initfragments(){
-
-        homeFragment = supportFragmentManager
-            .fragmentFactory.instantiate(classLoader, HomeFragment::class.java.name) as HomeFragment
-        favoriteFragment = supportFragmentManager
-            .fragmentFactory.instantiate(classLoader, FavoriteFragment::class.java.name) as FavoriteFragment
-        historyFragment = supportFragmentManager
-            .fragmentFactory.instantiate(classLoader, HistoryFragment::class.java.name) as HistoryFragment
-        moreFragment = supportFragmentManager
-            .fragmentFactory.instantiate(classLoader, MoreFragment::class.java.name) as MoreFragment
     }
 
 }
